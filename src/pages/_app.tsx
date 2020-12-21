01166be62c5652a393c9dfb26795d3b1d19b57c0
Rodrigo <rodrigo.hammes7@gmail.com>
2020-12-21 12:59:53 -0300

    Paths no Typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import GlobalStyle from "../styles/GlobalStyle";
+import GlobalStyle from "@/styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }) {
   return(
--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 import { useState } from 'react';
 
 const AddToCartModal = dynamic(
-  () => import('../../../components/AddToCartModal'),
+  () => import('@/components/AddToCartModal'),
   { 
     loading: () => <p>Carregando...</p> 
   }
