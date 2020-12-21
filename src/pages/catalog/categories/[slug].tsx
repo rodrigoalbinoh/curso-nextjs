57d72c615501d554698a9880d3a901a617daa761
Rodrigo <rodrigo.hammes7@gmail.com>
2020-12-20 21:19:44 -0300

    Dynamic Static Pages

---
+++ src/pages/catalog/categories/[slug].tsx
@@ -0,0 +1,65 @@
+import { useRouter } from 'next/router'
+import { GetStaticProps, GetStaticPaths } from 'next';
+
+
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface CategoryProps {
+  products: IProduct[]
+}
+
+export default function Category({ products }: CategoryProps) {
+  const router = useRouter()
+
+  return (
+    <>
+      <h1>{router.query.slug}</h1>
+      <ul>
+        {products.map(product => {
+          return (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('http://localhost:3333/categories');
+
+  const categories = await response.json();
+
+  const paths = categories.map(category => {
+    return {
+      params: {
+        slug: category.id
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async (context) => {
+  const { slug } = context.params
+  
+  const response = await fetch(`http://localhost:3333/products?category_id=${slug}`);
+
+  const products = await response.json();
+  
+  return {
+    props: {
+      products
+    },
+    revalidate: 60
+  }
+}
